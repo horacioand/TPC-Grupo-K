c84c75c79c769976fca5565aed45fb46f1e99849
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2025-01-23 10:31:58 -0600

    Panel de control front

---
+++ Visual/PanelControl.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="Panel de control" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="PanelControl.aspx.cs" Inherits="Visual.PanelControl" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MasterSite" runat="server">
+    <h3 class="contenedor">Panel de control</h3>
+    <div class="text-center contenedor">
+        <div class="card text-center mb-3 margen" style="width: 18rem;">
+            <div class="card-body divpanelcontrol">
+                <h5 class="card-title">Meseros</h5>
+                <asp:Button ID="btnAgregarMesero" CssClass="btn btn-primary margen" runat="server" Text="Agregar" />
+                <asp:Button ID="btnModificarMesero" CssClass="btn btn-primary margen" runat="server" Text="Modificar" />
+                <asp:Button ID="btnEliminarMesero" CssClass="btn btn-primary margen" runat="server" Text="Eliminar" />
+            </div>
+        </div>
+        <div class="card text-center mb-3 margen" style="width: 18rem;">
+            <div class="card-body divpanelcontrol">
+                <h5 class="card-title">Platos</h5>
+                <asp:Button ID="btnAgregarPlato" CssClass="btn btn-primary margen" runat="server" Text="Agregar" />
+                <asp:Button ID="btnModificarPlato" CssClass="btn btn-primary margen" runat="server" Text="Modificar" />
+                <asp:Button ID="btnEliminarPlato" CssClass="btn btn-primary margen" runat="server" Text="Eliminar" />
+            </div>
+        </div>
+        <div class="card text-center mb-3 margen" style="width: 18rem;">
+            <div class="card-body divpanelcontrol">
+                <h5 class="card-title">Informes</h5>
+                <asp:Button ID="btnVentas" CssClass="btn btn-primary margen" runat="server" Text="Ventas" />
+            </div>
+        </div>
+    </div>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <asp:Button ID="btnAsignarMesas" CssClass="btn btn-primary margen" runat="server" Text="Asignar Mesas" />
+    </div>
+</asp:Content>
---
+++ Visual/PanelControl.aspx.designer.cs
@@ -0,0 +1,89 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Visual
+{
+
+
+    public partial class PanelControl
+    {
+
+        /// <summary>
+        /// Control btnAgregarMesero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarMesero;
+
+        /// <summary>
+        /// Control btnModificarMesero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificarMesero;
+
+        /// <summary>
+        /// Control btnEliminarMesero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarMesero;
+
+        /// <summary>
+        /// Control btnAgregarPlato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarPlato;
+
+        /// <summary>
+        /// Control btnModificarPlato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnModificarPlato;
+
+        /// <summary>
+        /// Control btnEliminarPlato.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnEliminarPlato;
+
+        /// <summary>
+        /// Control btnVentas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnVentas;
+
+        /// <summary>
+        /// Control btnAsignarMesas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAsignarMesas;
+    }
+}
